569e83872c3dc1c554c7d30ee022fdcae366ffb4
Adrian Bowyer <adrian@reprappro.com>
2019-02-11 17:30:41 +0000

    Tuning up the light detection.

--- Software/reprap-multifilament/reprap-multifilament.ino
+++ Software/reprap-multifilament/reprap-multifilament.ino
@@ -1,20 +1,20 @@
 /*
- * RepRap MultiFilament Arduino Control Program
- * 
- * This controls the RepRap Multifilament drive using
- * a shield for the Arduino Uno.
- * 
- * Adrian Bowyer
- * 
- * RepRap Ltd
- * http://reprapltd.com
- * 
- * Github: https://github.com/RepRapLtd/RepRap-MultiFilament
- * 
- * Licence: GPL
- * 
- * 7 February 2019
- */
+   RepRap MultiFilament Arduino Control Program
+
+   This controls the RepRap Multifilament drive using
+   a shield for the Arduino Uno.
+
+   Adrian Bowyer
+
+   RepRap Ltd
+   http://reprapltd.com
+
+   Github: https://github.com/RepRapLtd/RepRap-MultiFilament
+
+   Licence: GPL
+
+   7 February 2019
+*/
 
 // Arduino Uno Pins
 
@@ -37,8 +37,20 @@ const int drive = 1;
 const int guillotine = 2;
 const int forward = 1;
 const int reverse = 2;
-const int findThreshold = 100;
-const int moveThreshold = 140;
+//const int findThreshold = 100;
+//const int moveThreshold = 140;
+const long maxMoveTime = 16000;
+
+/*
+ * The expected difference between light and dark reduces as ambient light
+ * increases.  This is an approximation to that behaviour:
+ * 
+ *    Difference = -0.0002948xdark_level^2 + 0.4343xdark_level + 0.05407
+ */
+
+ const float a0 = 0.05407;
+ const float a1 = 0.4343;
+ const float a2 = -0.0002948;
 
 // Status pin
 
@@ -48,64 +60,62 @@ const unsigned char debugPin = 2;  // On the //-lel port; Ground this pin to tur
 // PWM
 
 unsigned char forwardPWM = 170;
-unsigned char reversePWM = 50;
+unsigned char reversePWM = 80;
 bool debug;
 int lastDirection;
 
 /*
- * Control the movement of the motors
- */
+   Control the movement of the motors
+*/
 void Brake(int device)
 {
-  if(device == drive)
+  if (device == drive)
   {
     digitalWrite(driveFIN, 1);
-    digitalWrite(driveRIN, 1);    
+    digitalWrite(driveRIN, 1);
   } else
   {
     digitalWrite(guillotineFIN, 1);
-    digitalWrite(guillotineRIN, 1);       
-  } 
+    digitalWrite(guillotineRIN, 1);
+  }
 }
 
 void Forward(int device)
 {
   lastDirection = forward;
-  if(device == drive)
+  if (device == drive)
   {
     analogWrite(driveFIN, forwardPWM);
-    digitalWrite(driveRIN, 0);    
+    digitalWrite(driveRIN, 0);
   } else
   {
     digitalWrite(guillotineFIN, 1);
-    analogWrite(guillotineRIN, 255 - forwardPWM);       
-  }   
+    analogWrite(guillotineRIN, 255 - forwardPWM);
+  }
 }
 
 void Reverse(int device)
 {
   lastDirection = reverse;
-  if(device == drive)
+  if (device == drive)
   {
     analogWrite(driveFIN, 255 - reversePWM);
-    digitalWrite(driveRIN, 1);    
+    digitalWrite(driveRIN, 1);
   } else
   {
     digitalWrite(guillotineFIN, 0);
-    analogWrite(guillotineRIN, reversePWM);       
-  }     
+    analogWrite(guillotineRIN, reversePWM);
+  }
 }
 
 
+
 /*
- * Get the difference between dark and light for a LED/phototransistor pair.
- * Note dark reading are larger than light ones.
+ * Set up a LED light reading with the right phototransistor and LED
  */
-int differentialPhoto(int device, int ledNumber)
+void GetTransistorAndLED(int device, int ledNumber, unsigned char& pt, unsigned char& led)
 {
-  unsigned char pt;
-  unsigned char led;
-  if(device == drive)
+  if (device == drive)
   {
     pt = drivePT;
     led = driveLED[ledNumber];
@@ -113,8 +123,19 @@ int differentialPhoto(int device, int ledNumber)
   {
     pt = guillotinePT;
     led = guillotineLED[ledNumber];
-  }
-  
+  }  
+}
+
+/*
+   Get the difference between dark and light for a LED/phototransistor pair.
+   Note dark readings are larger than light ones.
+*/
+int differentialPhoto(int device, int ledNumber)
+{
+  unsigned char pt;
+  unsigned char led;
+  GetTransistorAndLED(device, ledNumber, pt, led);
+
   int result = analogRead(pt);
   digitalWrite(led, 1);
   result = result - analogRead(pt);
@@ -123,17 +144,54 @@ int differentialPhoto(int device, int ledNumber)
 }
 
 /*
- * Find what position the device is at. -1 means its phototransistor
- * is not opposite any LED.  Note this is for when the device is stationary; it won't
- * work well when the device is moving.
+ * Are we ar a given position?
+ * The criterion is LED on/off difference is 80% of the expected value.
  */
+bool atLEDPosition(int device, int ledNumber)
+{
+  unsigned char pt;
+  unsigned char led;
+  GetTransistorAndLED(device, ledNumber, pt, led);
+
+  int dark = analogRead(pt);
+  digitalWrite(led, 1);
+  int diff = dark - analogRead(pt);
+  digitalWrite(led, 0);
+  //float expectedDiff = darkGradient*(float)dark + darkIntercept;
+  float expectedDiff = (float)dark*(float)dark;
+  expectedDiff = a2*expectedDiff + a1*(float)dark + a0;
+  Serial.println(expectedDiff);
+  return diff > (int)(0.5 + 0.8*expectedDiff);  
+}
+
+void ReportDarkAndLight(int device, int ledNumber)
+{
+  unsigned char pt;
+  unsigned char led;
+  GetTransistorAndLED(device, ledNumber, pt, led);
+
+  int dark = analogRead(pt);
+  digitalWrite(led, 1);
+  int light = analogRead(pt);
+  digitalWrite(led, 0);
+  Serial.print("Dark: ");
+  Serial.print(dark);
+  Serial.print(", light: ");
+  Serial.print(light);
+  Serial.print(", diff: ");
+  Serial.println(dark - light);  
+}
+
+/*
+   Find what position the device is at. -1 means its phototransistor
+   is not opposite any LED.  Note this is for when the device is stationary; it won't
+   work well when the device is moving.
+*/
 int FindPosition(int device)
 {
-  int result = -1;
-  int reading, maxReading;
   int leds;
-  
-  if(device == drive)
+
+  if (device == drive)
   {
     leds = driveLEDs;
   } else
@@ -141,108 +199,97 @@ int FindPosition(int device)
     leds = guillotineLEDs;
   }
 
-  if(debug)
+  if (debug)
   {
     Serial.print("Finding position. diff V: ");
   }
-  
-  maxReading = -1;
-  for(int led = 0; led < leds; led++)
+
+  int led = 0;
+  while(led < leds && !atLEDPosition(device, led))
   {
-    reading = differentialPhoto(device, led);
-    if(debug)
-    {
-      Serial.print(led);
-      Serial.print(":");
-      Serial.print(reading);
-      Serial.print(", ");
-    }
-    
-    if(reading > findThreshold)
-    {
-      if(reading > maxReading)
-      {
-        result = led;
-        maxReading = reading;
-      }
-    }
+    led++;
   }
 
-  if(debug)
+  if(led >= leds)
+  {
+    led = -1;
+  }
+
+  if (debug)
   {
     Serial.print(" pos: ");
-    Serial.println(result);
+    Serial.println(led);
   }
-  return result;
+  return led;
 }
 
 /*
- * Continue the last ovement a bit to maximise the phototransistor output and
- * so centre the LED and phototransistor opposite each other.
- */
+   Continue the last ovement a bit to maximise the phototransistor output and
+   so centre the LED and phototransistor opposite each other.
+*/
 void FineTune(int device, int led)
 {
-/*  float sensorLevel = (float)differentialPhoto(device, led);
-  if(lastDirection == forward)
-  {
-    Forward(device);
-  } else
-  {
-    Reverse(device);
-  }
-  delay(100);
-  float sl = 0.0;
-  bool move = true;
-  bool count = 0;
-  while(move)
-  {
-    count++;
-    sl += differentialPhoto(device, led);
-    if(!(count%5))
+  /*  float sensorLevel = (float)differentialPhoto(device, led);
+    if(lastDirection == forward)
     {
-      move = false;
-      count = 0;
-      sl = sl*0.2;
-      if(sl > sensorLevel)
+      Forward(device);
+    } else
+    {
+      Reverse(device);
+    }
+    delay(100);
+    float sl = 0.0;
+    bool move = true;
+    bool count = 0;
+    while(move)
+    {
+      count++;
+      sl += differentialPhoto(device, led);
+      if(!(count%5))
       {
-        sensorLevel = sl;
-        move = true;
+        move = false;
+        count = 0;
+        sl = sl*0.2;
+        if(sl > sensorLevel)
+        {
+          sensorLevel = sl;
+          move = true;
+        }
       }
     }
-  }
-  Brake(device);*/
+    Brake(device);*/
 }
 
 /*
- * Drive the device to a given position.
- */
+   Drive the device to a given position.
+*/
 int SeekPosition(int device, int led)
 {
-  if(debug)
+  if (debug)
   {
     Serial.print("Seeking position ");
     Serial.print(led);
     Serial.print(" on device ");
     Serial.println(device);
   }
-  
+
   int pos = FindPosition(device);
-  if(pos < 0)
+  if (pos < 0)
   {
-    if(debug)
+    if (debug)
     {
       Serial.println("Seeking a position from an unknown position!");
       return;
     }
   }
-  
-  if(pos == led)
+
+  if (pos == led)
   {
     return;
     //FineTune(device, led);
   }
-  
-  if(pos > led)
+
+  if (pos > led)
   {
     Reverse(device);
   } else
@@ -252,38 +299,48 @@ int SeekPosition(int device, int led)
 
   long t = (long)millis();
   bool notThere = true;
-  while((long)millis() - t < 10000 && notThere)
+
+  while ((((long)millis() - t) < maxMoveTime) && notThere)
   {
-    if(differentialPhoto(device, led) > moveThreshold)
+    if(atLEDPosition(device, led))
     {
       Brake(device);
       notThere = false;
     }
+    delay(10);
   }
   Brake(device);
+  
+  if (debug)
+  {
+    if( notThere)
+    {
+      Serial.println("Time out on movement.");
+    }
+  }
   FineTune(device, led);
 }
 
 
 
-void setup() 
+void setup()
 {
   pinMode(busy, OUTPUT);
   digitalWrite(busy, 0);
-  
+
   pinMode(driveFIN, OUTPUT);
   pinMode(driveRIN, OUTPUT);
   pinMode(guillotineFIN, OUTPUT);
   pinMode(guillotineRIN, OUTPUT);
   Brake(drive);
   Brake(guillotine);
-  
-  for(int i = 0; i < driveLEDs; i++)
+
+  for (int i = 0; i < driveLEDs; i++)
   {
     pinMode(driveLED[i], OUTPUT);
     digitalWrite(driveLED[i], 0);
   }
-  for(int i = 0; i < guillotineLEDs; i++)
+  for (int i = 0; i < guillotineLEDs; i++)
   {
     pinMode(guillotineLED[i], OUTPUT);
     digitalWrite(guillotineLED[i], 0);
@@ -292,10 +349,10 @@ void setup()
   pinMode(debugPin, INPUT_PULLUP);
   debug = !digitalRead(debugPin);
   pinMode(guillotinePT, INPUT);
-  pinMode(drivePT, INPUT);  
-  
+  pinMode(drivePT, INPUT);
+
   Serial.begin(9600);
-  if(debug)
+  if (debug)
   {
     Serial.println("\nRepRap MultiFilament\n");
   }
@@ -304,73 +361,85 @@ void setup()
 
 }
 
-void loop() 
+void loop()
 {
   int device = guillotine;
-  if(Serial.available())
+  int p;
+  
+  if (Serial.available())
   {
     int c = Serial.read();
-    switch(c)
+    switch (c)
     {
-     case ' ':
-     case '\t':
-     case '\n':
-      break;
-      
-     case '0':
-      SeekPosition(device, 0);
-      break;
+      case ' ':
+      case '\t':
+      case '\n':
+        break;
 
-     case '1':
-      SeekPosition(device, 1);
-      break;
+      case '0':
+        SeekPosition(device, 0);
+        break;
 
-     case 'f':
-      Forward(device);
-      delay(500);
-      Brake(device);
-      break;
+      case '1':
+        SeekPosition(device, 1);
+        break;
 
-     case 'r':
-      Reverse(device);
-      delay(500);
-      Brake(device);
-      break;
+      case 'f':
+        Forward(device);
+        delay(500);
+        Brake(device);
+        break;
 
-     case 'g':
-      SeekPosition(device, 1);
-      SeekPosition(device, 0);
-      break;
+      case 'r':
+        Reverse(device);
+        delay(500);
+        Brake(device);
+        break;
 
-     case 'F':
-      Forward(device);
-      delay(2000);
-      Brake(device);
-      break;
+      case 'g':
+        SeekPosition(device, 1);
+        SeekPosition(device, 0);
+        Reverse(device);
+        delay(3000);
+        Brake(device);
+        break;
 
-     case 'R':
-      Reverse(device);
-      delay(2000);
-      Brake(device);
-      break;
+      case 'F':
+        Forward(device);
+        delay(2000);
+        Brake(device);
+        break;
 
-     case 'p':
-      int p = FindPosition(device);
-      Serial.print("Position: ");
-      Serial.println(p);
-      break;
+      case 'R':
+        Reverse(device);
+        delay(2000);
+        Brake(device);
+        break;
 
-     default:
-      if(debug)
-      {
-        Serial.print("Illegal command: ");
-        Serial.println((char)c);
-      }
+      case 'p':
+        p = FindPosition(device);
+        Serial.print("Position: ");
+        Serial.println(p);
+        break;
+
+      case 'd':
+        while(!Serial.available());
+        c = Serial.read();
+        p = c - '0';
+        ReportDarkAndLight(device, p);
+        break;
+
+      default:
+        if (debug)
+        {
+          Serial.print("Illegal command: ");
+          Serial.println((char)c);
+        }
     }
   }
-  
+
 
   //Reverse(guillotine);
-  
+
   debug = !digitalRead(debugPin);
 }
